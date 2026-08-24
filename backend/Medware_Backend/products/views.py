2e14dd5d07929f920ace86b46d69623da8ae9947
Abdulkhalek Jahjah <jabdulkhalek4@gmail.com>
2026-08-24 23:14:31 +0300

    akj

--- backend/Medware_Backend/products/views.py
+++ backend/Medware_Backend/products/views.py
@@ -1,9 +1,8 @@
 from rest_framework import viewsets, permissions
-from .models import Supplier, Product, Bill, Archive, Order, OrderProduct, ProductSupplier, Voucher, OrderVoucher
+from .models import Supplier, Product, ProductSupplier
 from .serializers import (
-    SupplierSerializer, ProductSerializer, BillSerializer,
-    ArchiveSerializer, OrderSerializer, OrderProductSerializer,
-    ProductSupplierSerializer, VoucherSerializer, OrderVoucherSerializer
+    SupplierSerializer, ProductSerializer,
+    ProductSupplierSerializer
 )
 
 class SupplierViewSet(viewsets.ModelViewSet):
@@ -16,37 +15,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [permissions.AllowAny]
 
-class BillViewSet(viewsets.ModelViewSet):
-    queryset = Bill.objects.all()
-    serializer_class = BillSerializer
-    permission_classes = [permissions.AllowAny]
-
-class ArchiveViewSet(viewsets.ModelViewSet):
-    queryset = Archive.objects.all()
-    serializer_class = ArchiveSerializer
-    permission_classes = [permissions.AllowAny]
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [permissions.AllowAny]
-
-class OrderProductViewSet(viewsets.ModelViewSet):
-    queryset = OrderProduct.objects.all()
-    serializer_class = OrderProductSerializer
-    permission_classes = [permissions.AllowAny]
-
 class ProductSupplierViewSet(viewsets.ModelViewSet):
     queryset = ProductSupplier.objects.all()
     serializer_class = ProductSupplierSerializer
-    permission_classes = [permissions.AllowAny]
-
-class VoucherViewSet(viewsets.ModelViewSet):
-    queryset = Voucher.objects.all()
-    serializer_class = VoucherSerializer
-    permission_classes = [permissions.AllowAny]
-
-class OrderVoucherViewSet(viewsets.ModelViewSet):
-    queryset = OrderVoucher.objects.all()
-    serializer_class = OrderVoucherSerializer
     permission_classes = [permissions.AllowAny]
